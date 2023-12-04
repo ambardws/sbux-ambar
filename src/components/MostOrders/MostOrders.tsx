8282540c6a94aa273ab1db64eb0125839f09fc19
Ambar Dwi Saputra <ambardwisaputra@gmail.com>
2023-12-04 14:56:08 +0700

    fix discount: most orders list

--- src/components/MostOrders/MostOrders.tsx
+++ src/components/MostOrders/MostOrders.tsx
@@ -5,8 +5,13 @@ import { numberFormat } from "@trex/utils/format";
 import Image from "next/image";
 import React, { useEffect, useState } from "react";
 
+type OrdersCard = {
+  index: number;
+  order: MostOrdersInterface;
+};
+
 export default function MostOrders() {
-  const [orders, setOrders] = useState<MostOrdersInterface[]>();
+  const [orders, setOrders] = useState<MostOrdersInterface[]>([]);
 
   const getMostOrders = async () => {
     try {
@@ -19,6 +24,79 @@ export default function MostOrders() {
     }
   };
 
+  const CardMostOrder = (props: OrdersCard) => {
+    return (
+      <div
+        className={`card bg-base-100 shadow-md p-4 min-w-[170px] relative inline-block mt-[10px] ml-[10px] ${
+          orders.length - 1 === props.index ? "!mr-5" : ""
+        }`}
+      >
+        {props.order.discount !== 0 && (
+          <div className="stack-top left font-semibold text-sm">
+            {props.order.discount}%
+          </div>
+        )}
+
+        <div className="px-5 py-1 relative">
+          <img
+            src={`${props.order.image}`}
+            alt={props.order.coffee}
+            className="h-20 w-max m-auto"
+          />
+          {props.order.new === true && (
+            <div>
+              <Image
+                src={assets.StarbuckIcons.New}
+                alt="new"
+                className="h-14 w-14 absolute right-0 bottom-[-30px]"
+              />
+              <p className="absolute text-xs font-medium right-1 bottom-[-20px] rotate-[-45deg] text-white">
+                NEW!
+              </p>
+            </div>
+          )}
+        </div>
+
+        <div className="mt-8">
+          <h2 className="text-sm font-normal line-clamp-2 h-10">
+            {props.order.coffee}
+          </h2>
+          {props.order.discount !== 0 ? (
+            <div className="flex space-x-2">
+              <p className="mt-2 text-xs font-light text-[#BEC9E0] line-through">
+                {numberFormat({
+                  num: Number(props.order.price),
+                  opt: { style: "currency" },
+                })}
+              </p>
+              <p className="mt-2 text-xs font-bold">
+                {numberFormat({
+                  num: Number(props.order.price - props.order.price * 0.1),
+                  opt: { style: "currency" },
+                })}
+              </p>
+            </div>
+          ) : (
+            <p className="mt-2 text-xs font-bold">
+              {numberFormat({
+                num: Number(props.order.price),
+                opt: { style: "currency" },
+              })}
+            </p>
+          )}
+
+          <button className="btn btn-sm bg-gradient-to-r from-[#008950] to-[#007042] capitalize text-white text-xs w-full rounded-[50px] mt-2">
+            <Image
+              src={assets.StarbuckMostOrders.ShoppingCart}
+              alt="add-to-cart"
+            />
+            Add to Cart
+          </button>
+        </div>
+      </div>
+    );
+  };
+
   useEffect(() => {
     getMostOrders();
   }, []);
@@ -27,70 +105,7 @@ export default function MostOrders() {
       <h1 className="text-[#3C405F] font-medium text-base">Most Orders</h1>
       <div className="flex space-x-3 overflow-x-auto no-scrollbar -mr-5 pb-1 h-[270px] \">
         {orders?.map((order, index) => (
-          <div
-            key={index}
-            className={`card bg-base-100 shadow-md p-4 min-w-[170px] relative inline-block mt-[10px] ml-[10px] ${
-              orders.length - 1 === index ? "!mr-5" : ""
-            }`}
-          >
-            <div className="stack-top left font-semibold text-sm">{order.discount}%</div>
-            <div className="px-5 py-1 relative">
-              <img
-                src={`${order.image}`}
-                alt={order.coffee}
-                className="h-20 w-max m-auto"
-              />
-              {order.new === true && (
-                <div>
-                  <Image
-                    src={assets.StarbuckIcons.New}
-                    alt="new"
-                    className="h-14 w-14 absolute right-0 bottom-[-30px]"
-                  />
-                  <p className="absolute text-xs font-medium right-1 bottom-[-20px] rotate-[-45deg] text-white">
-                    NEW!
-                  </p>
-                </div>
-              )}
-            </div>
-
-            <div className="mt-8">
-              <h2 className="text-sm font-normal line-clamp-2 h-10">
-                {order.coffee}
-              </h2>
-              {order.discount !== 0 ? (
-                <div className="flex space-x-2">
-                  <p className="mt-2 text-xs font-light text-[#BEC9E0] line-through">
-                    {numberFormat({
-                      num: Number(order.price),
-                      opt: { style: "currency" },
-                    })}
-                  </p>
-                  <p className="mt-2 text-xs font-bold">
-                    {numberFormat({
-                      num: Number(order.price - order.price * 0.1),
-                      opt: { style: "currency" },
-                    })}
-                  </p>
-                </div>
-              ) : (
-                <p className="mt-2 text-xs font-bold">
-                  {numberFormat({
-                    num: Number(order.price),
-                    opt: { style: "currency" },
-                  })}
-                </p>
-              )}
-
-              <button className="btn btn-sm bg-gradient-to-r from-[#008950] to-[#007042] capitalize text-white text-xs w-full rounded-[50px] mt-2">
-                <Image
-                  src={assets.StarbuckMostOrders.ShoppingCart}
-                  alt="add-to-cart"
-                />
-                Add to Cart
-              </button>
-            </div>
-          </div>
+          <CardMostOrder order={order} index={index} key={index} />
         ))}
       </div>
     </div>
