b45f54e66a7ddc4aadd5f78cf269a306e79c0442
Ambar Dwi Saputra <ambardwisaputra@gmail.com>
2023-12-13 19:49:05 +0700

    feat: fix state rerender cart

--- src/components/BottomSheet/BottomSheet.tsx
+++ src/components/BottomSheet/BottomSheet.tsx
@@ -57,7 +57,9 @@ export const BottomSheet = (props: any) => {
     };
 
     const handleSaveNotes = (id: number, notes: string) => {
-      updateNotes(id, notes);
+      if (notes) {
+        updateNotes(id, notes);
+      }
       setEditableNote(false);
     };
     return (
@@ -125,12 +127,25 @@ export const BottomSheet = (props: any) => {
                             className="input input-sm input-ghost pl-0 text-[#3C405F] text-xs !bg-white !border-none !cursor-default"
                             defaultValue={item.notes}
                           />
-                          <Image
-                            src={assets.StarbuckCart.Edit}
-                            alt="edit-cart"
-                            className="h-8 w-8 cursor-pointer"
+                          <div
+                            className="cursor-pointer border border-[#82BC3F] rounded-[10px] h-8 w-8 flex items-center justify-center"
                             onClick={() => handleEdit()}
-                          />
+                          >
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                              stroke-width="1.5"
+                              stroke="currentColor"
+                              className="w-5 h-5 text-[#82BC3F]"
+                            >
+                              <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125"
+                              />
+                            </svg>
+                          </div>
                         </div>
                       ) : (
                         <div className="flex justify-between items-end">
@@ -143,12 +158,25 @@ export const BottomSheet = (props: any) => {
                             className="input input-sm input-ghost pl-0 text-[#3C405F] italic text-xs !bg-white !border-none !cursor-default"
                             defaultValue={"No Notes"}
                           />
-                          <Image
-                            src={assets.StarbuckCart.Edit}
-                            alt="edit-cart"
-                            className="h-8 w-8 cursor-pointer"
+                          <div
+                            className="cursor-pointer border border-[#82BC3F] rounded-[10px] h-8 w-8 flex items-center justify-center"
                             onClick={() => handleEdit()}
-                          />
+                          >
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                              stroke-width="1.5"
+                              stroke="currentColor"
+                              className="w-5 h-5 text-[#82BC3F]"
+                            >
+                              <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125"
+                              />
+                            </svg>
+                          </div>
                         </div>
                       )}
                     </div>
@@ -156,12 +184,25 @@ export const BottomSheet = (props: any) => {
                 </div>
               </div>
               <div className="flex space-x-3 pr-5 items-end">
-                <Image
-                  src={assets.StarbuckCart.Delete}
-                  alt="delete-orders"
-                  className="h-8 w-8 cursor-pointer"
+                <div
+                  className="cursor-pointer border border-[#3C405F] rounded-[10px] h-8 w-8 flex items-center justify-center"
                   onClick={() => handleDelete(item.id)}
-                />
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    className="w-5 h-5 text-[#3C405F]"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                    />
+                  </svg>
+                </div>
                 <button
                   disabled={item.qty === 1}
                   className={clsx(
