ad3f4f1199274fe134cf5bd755dcff40fbd591aa
Ambar Dwi Saputra <ambardwisaputra@gmail.com>
2023-12-13 20:21:56 +0700

    fix: fix constant

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,23 +1,22 @@
-import { API_BASE_URL } from "@/constants/env";
+// import { API_BASE_URL } from "@/constants/env";
 
 export const getInitialName = (name: string) => {
-    let names = String(name || '')
-      .trim()
-      .replace('[', '')
-      .replace('(', '')
-      .split(' ');
-    let initials = names[0].substring(0, 1).toUpperCase();
-  
-    if (names.length > 1) {
-      initials += names[names.length - 1].substring(0, 1).toUpperCase();
-    }
-    return initials;
-  };
-  
+  let names = String(name || "")
+    .trim()
+    .replace("[", "")
+    .replace("(", "")
+    .split(" ");
+  let initials = names[0].substring(0, 1).toUpperCase();
 
-  export const getImageUrl = (url: string) => {
-    if (url.includes('http')) {
-        return url;
-    }
-    return API_BASE_URL + url
+  if (names.length > 1) {
+    initials += names[names.length - 1].substring(0, 1).toUpperCase();
   }
+  return initials;
+};
+
+// export const getImageUrl = (url: string) => {
+//   if (url.includes('http')) {
+//       return url;
+//   }
+//   return API_BASE_URL + url
+// }
